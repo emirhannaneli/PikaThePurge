d7068b73ecbcf50ac57ef97ba593911e9bc21fd8
PikaDev <70511355+PiikaDev@users.noreply.github.com>
2021-06-12 19:24:12 +0300

    Bug Fixed

--- src/main/java/xyz/pikadev/PikaThePurge.java
+++ src/main/java/xyz/pikadev/PikaThePurge.java
@@ -1,11 +1,8 @@
 package xyz.pikadev;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.WorldBorder;
-import org.bukkit.boss.BossBar;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.pikadev.Commands.PTP;
 import xyz.pikadev.Donate.Book;
@@ -13,6 +10,7 @@ import xyz.pikadev.Events.*;
 
 public final class PikaThePurge extends JavaPlugin {
     public static boolean gameStatus = false;
+
     @Override
     public void onEnable() {
         /*Create Config*/
@@ -23,12 +21,12 @@ public final class PikaThePurge extends JavaPlugin {
         /*Register Tab Completions*/
         getCommand("ptp").setTabCompleter(new TabCompleitons());
         /*Register Events*/
-        getServer().getPluginManager().registerEvents(new OnDeath(),this);
-        getServer().getPluginManager().registerEvents(new OnJoin(),this);
-        getServer().getPluginManager().registerEvents(new OnDamage(),this);
-        getServer().getPluginManager().registerEvents(new OnPortal(),this);
-        getServer().getPluginManager().registerEvents(new OnQuit(),this);
-        getServer().getPluginManager().registerEvents(new Book(),this);
+        getServer().getPluginManager().registerEvents(new OnDeath(), this);
+        getServer().getPluginManager().registerEvents(new OnJoin(), this);
+        getServer().getPluginManager().registerEvents(new OnDamage(), this);
+        getServer().getPluginManager().registerEvents(new OnPortal(), this);
+        getServer().getPluginManager().registerEvents(new OnQuit(), this);
+        getServer().getPluginManager().registerEvents(new Book(), this);
         /*World Border*/
         setBorder();
         /*Metrics*/
@@ -37,13 +35,13 @@ public final class PikaThePurge extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage("§6Pika§eThePurge » §aAktif...");
     }
 
-    private void setBorder(){
-        if(getConfig().getBoolean("World.Border")){
+    private void setBorder() {
+        if (getConfig().getBoolean("World.Border")) {
             String worldName = getConfig().getString("World.Name");
             double borderSize = getConfig().getInt("RandomTeleportRadius.Max") * 1.2;
-            Bukkit.getWorld(worldName).setSpawnLocation(new Location(Bukkit.getWorld(worldName),0,0,0));
+            Bukkit.getWorld(worldName).setSpawnLocation(new Location(Bukkit.getWorld(worldName), 0, 0, 0));
             WorldBorder wb = Bukkit.getWorld(worldName).getWorldBorder();
-            wb.setCenter(new Location(Bukkit.getWorld(worldName),0,0,0));
+            wb.setCenter(new Location(Bukkit.getWorld(worldName), 0, 0, 0));
             wb.setSize(borderSize);
         }
     }
